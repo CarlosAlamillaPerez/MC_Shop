eebe86dd38a558661b4b6e28ef49eab31493881e
CarlosAlamillaPerez <carlosalamillap@gmail.com>
2024-08-07 10:25:44 -0600

    First commit V.0.0

---
+++ Models/ArticuloModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MC_Shop.Models
+{
+    public class ArticuloModel
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Precio { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public string ImagenPrincipal { get; set; } = string.Empty;
+        public List<string> Imagenes { get; set; } = new List<string>();
+        public string DescriptionCard { get; set; } = string.Empty;
+        public int Talla { get; set; }
+        public int Categoria { get; set; }
+    }
+}
---
+++ Services/ArticuloService.cs
@@ -0,0 +1,33 @@
+﻿using MC_Shop.Models;
+
+namespace MC_Shop.Services
+{
+    public class ArticuloService
+    {
+        public class ArticuloService
+        {
+            private List<ArticuloModel> _rooms;
+
+            public ArticuloService()
+            {
+                _rooms = new List<ArticuloModel>
+                {
+                    new ArticuloModel
+                    {
+                    
+                    },
+                };
+            }
+
+            public List<ArticuloModel> GetAllRooms()
+            {
+                return _rooms;
+            }
+
+            public ArticuloModel? GetRoom(int id)
+            {
+                return _rooms.FirstOrDefault(r => r.Id == id);
+            }
+        }
+    }
+}
